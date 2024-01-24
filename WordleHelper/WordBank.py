c5428fd533ddeb9f317c71dfee1bbad2a5d525a6
jue-henry <juehenry@yahoo.com>
2024-01-24 13:30:07 -0800

    Adds scoring to words and sorts word bank based on letter frequency in English, suggests words based on this scoring

--- WordleHelper/WordBank.py
+++ WordleHelper/WordBank.py
@@ -1,7 +1,41 @@
+from sortedcontainers import SortedList
+
+# points assigned to letters based on frequency of use in English language
+LETTER_RANKINGS = {
+    'e': 10,
+    't': 9,
+    'a': 8,
+    'o': 7.5,
+    'i': 7,
+    'n': 7,
+    's': 6.5,
+    'h': 6,
+    'r': 6,
+    'd': 4.5,
+    'l': 4,
+    'u': 3,
+    'c': 2.5,
+    'm': 2.5,
+    'f': 2.5,
+    'w': 2,
+    'g': 2,
+    'y': 2,
+    'p': 1.5,
+    'b': 1.5,
+    'v': 1,
+    'k': 0.5,
+    'x': 0.5,
+    'j': 0.5,
+    'q': 0.5,
+    'z': 0.5
+}
+
 class WordBank():
     def __init__(self):
         with open('words.txt') as word_file:
-            self.words = list(word_file.read().split())
+            word_list = word_file.read().split()
+           
+        self.words = list(reversed(SortedList(word_list, key = WordBank.get_score)))
            
     def process_guess(self, guess: str, feedback: str) -> None:
         try:
@@ -18,3 +52,16 @@ class WordBank():
                     else:
                         self.words = list(filter(lambda x:not guess[i] in x, self.words))
         print(self.words)
+        
+        if len(self.words):
+            print(self.words[0])
+
+    @staticmethod
+    # method to score words based on frequency of the letters used in the English language
+    def get_score(word: str) -> int:
+        score = 0
+        for i in range(len(word)):
+            # skips duplicate letters in order to increase variety of letters for suggestion
+            if not word[i] in word[:i]:
+                score += LETTER_RANKINGS[word[i]]
+        return score
