ab3cfc0de874c8b759ad605bfdfab3169b428b4a
jue-henry <juehenry@yahoo.com>
2024-01-24 13:29:13 -0800

    skips processing duplicate letters in guess

--- WordleHelper/WordBank.py
+++ WordleHelper/WordBank.py
@@ -12,8 +12,9 @@ class WordBank():
             for i in range(len(guess)):
                 if feedback[i] == 'G':
                     self.words = list(filter(lambda x:x[i] == guess[i], self.words))
-                elif feedback[i] == 'Y':
-                    self.words = list(filter(lambda x:x[i] != guess[i] and guess[i] in x, self.words))
-                else:
-                    self.words = list(filter(lambda x:not guess[i] in x, self.words))
+                elif not guess[i] in guess[:i]:
+                    if feedback[i] == 'Y':
+                        self.words = list(filter(lambda x:x[i] != guess[i] and guess[i] in x, self.words))
+                    else:
+                        self.words = list(filter(lambda x:not guess[i] in x, self.words))
         print(self.words)
